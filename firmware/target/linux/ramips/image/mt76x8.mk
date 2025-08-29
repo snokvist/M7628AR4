72da66cebfd412554cbb3e9749a0ca9cc87bd28a
Joakim <joakim.snokvist@gmail.com>
2025-08-29 23:47:55 +0200

    Update mt76x8.mk

--- firmware/target/linux/ramips/image/mt76x8.mk
+++ firmware/target/linux/ramips/image/mt76x8.mk
@@ -5,10 +5,10 @@ define Device/custom_m7628ar4
   DEVICE_VENDOR := Custom
   DEVICE_MODEL := M7628AR4
   DEVICE_PACKAGES := \
-	kmod-mt76 kmod-mt7603 \
-	kmod-mt7615e mt7663-firmware-ap mt7663-firmware-sta \
-	wpad-basic-mbedtls wireless-regdb \
-	luci-ssl-openssl luci-app-opkg \
-	ca-bundle ca-certificates curl
+        kmod-mt76 kmod-mt7603 \
+        kmod-mt7615e mt7663-firmware-ap mt7663-firmware-sta \
+        wpad-basic-mbedtls wireless-regdb \
+        luci-ssl luci-app-opkg \
+        ca-bundle ca-certificates curl
 endef
 TARGET_DEVICES += custom_m7628ar4
