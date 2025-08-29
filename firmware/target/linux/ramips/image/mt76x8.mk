80b0c4bfbfbb311cc515bb110cec1c40aa315b30
Joakim <joakim.snokvist@gmail.com>
2025-08-29 22:59:29 +0200

    Update mt76x8.mk

--- firmware/target/linux/ramips/image/mt76x8.mk
+++ firmware/target/linux/ramips/image/mt76x8.mk
@@ -1,10 +1,11 @@
 define Device/custom_m7628ar4
   SOC := mt7628an
+  DEVICE_DTS := mt7628an-m7628ar4   # <— tell it the real DTS filename
   IMAGE_SIZE := 16064k
   DEVICE_VENDOR := Custom
   DEVICE_MODEL := M7628AR4
   DEVICE_PACKAGES := \
-        kmod-mt76 wpad-basic-mbedtls \
-        kmod-mt7615e mt7663-firmware-ap mt7663-firmware-sta
+        kmod-mt76 kmod-mt7615e mt7663-firmware-ap mt7663-firmware-sta \
+        wpad-basic-mbedtls
 endef
 TARGET_DEVICES += custom_m7628ar4
