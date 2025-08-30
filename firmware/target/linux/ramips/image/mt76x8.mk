c3ecdec7317b19c6f17dad931f2c539c6a4bbce9
Joakim <joakim.snokvist@gmail.com>
2025-08-30 15:22:42 +0200

    Update mt76x8.mk

--- firmware/target/linux/ramips/image/mt76x8.mk
+++ firmware/target/linux/ramips/image/mt76x8.mk
@@ -30,7 +30,8 @@ define Device/custom_m7628ar4
     luci-light uhttpd uhttpd-mod-ubus \
     kmod-mt7603 kmod-mt7615e kmod-mt7615-common kmod-mt76-core \
     wpad-basic-wolfssl wireless-regdb \
-    kmod-zram zram-swap \
+    wfb-ng wfb-ng-tun kmod-usb2 kmod-usb-ohci kmod-usb-ehci kmod-usb-storage block-mount bash \
+    nano-tiny coreutils-timeout kmod-zram zram-swap \
     -kmod-mt76 -kmod-mt76x02-common -kmod-mt76x2 -kmod-mt76x2-common \
     -ppp -ppp-mod-pppoe -luci-proto-ppp \
     -odhcpd-ipv6only -odhcp6c -luci-proto-ipv6
